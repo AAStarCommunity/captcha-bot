b17d720f2dfdae38eae66d1c54e79c0dbd8392ad
徐超越 <993921@qq.com>
2024-03-17 13:32:33 +0800

    chore: prevent to sleep

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/assimon/captcha-bot/bootstrap"
+	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,6 +15,20 @@ func main() {
 			fmt.Fprintf(w, "OK")
 		})
 		http.ListenAndServe(":80", nil)
+	}()
+
+	go func() {
+		t := time.NewTimer(time.Second * 10)
+		for {
+			if resp, err := http.Get("https://captcha-bot-zg9p.onrender.com/healthz"); err != nil {
+				fmt.Println(fmt.Sprintf("err:%v", err))
+			} else {
+				if b, err := io.ReadAll(resp.Body); err == nil {
+					fmt.Println("health: " + string(b))
+				}
+			}
+			<-t.C
+		}
 	}()
 
 	bootstrap.Start()
