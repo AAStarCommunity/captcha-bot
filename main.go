b392da23633bdb255eed94d4dfd1272f2d132632
徐超越 <993921@qq.com>
2024-03-17 11:51:43 +0800

    chore: allow Access-Control-Allow-Origin when health check

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/assimon/captcha-bot/bootstrap"
+	"net/http"
 )
 
 func main() {
 	go func() {
 		http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			fmt.Fprintf(w, "OK")
 		})
 		http.ListenAndServe(":80", nil)
